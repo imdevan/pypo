53fa1923b23ba69e4147e1d37f8b7fc279b53986
imdevan <huapayadevan@gmail.com>
2025-07-30 16:21:01 -0700

    Add openapi-typescript to expo

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -6,7 +6,17 @@ set -x
 cd backend
 python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 cd ..
+
+# Copy openapi.json to expo and frontend
+cp openapi.json expo/openapi.json
 mv openapi.json frontend/
+
+# Generate frontend client
 cd frontend
 npm run generate-client
 npx biome format --write ./src/client
+
+# Generate expo client
+cd ../expo
+bun generate:client
+bunx biome format --write ./app/client

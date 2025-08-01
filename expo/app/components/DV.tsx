a84b60205f8fab840df8a0c326d1bc49b8e8fbf5
imdevan <huapayadevan@gmail.com>
2025-08-01 16:51:56 -0700

    Add DebugView DV component

---
+++ expo/app/components/DV.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, forwardRef, ForwardedRef } from "react"
+// eslint-disable-next-line no-restricted-imports
+import { StyleProp, Text as RNText, TextProps as RNTextProps, TextStyle } from "react-native"
+import { View as RNView, ViewProps as RNViewProps, ViewStyle } from "react-native"
+
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle, ThemedStyleArray } from "@/theme/types"
+import { V, VProps } from "./V"
+import { DebugMode, useDebugStore } from "@/stores/debugStore"
+
+type Presets = "default" 
+
+export interface DVProps extends VProps {
+  debugLevel?: DebugMode
+}
+
+/**
+ * Debug View
+ * A view that only renders if the debug mode is enabled.
+ * @param props - The props for the DV component.
+ * @param ref - The ref for the DV component.
+ * @returns The DV component.
+ */
+export const DV = forwardRef(function DV({debugLevel = DebugMode.BASIC, ...props}: DVProps, ref: ForwardedRef<RNView>) {
+  const {debugMode, isDebugEnabled} = useDebugStore();
+
+  if (!isDebugEnabled()) {
+    return null;
+  }
+
+  return <V {...props} ref={ref} />
+})

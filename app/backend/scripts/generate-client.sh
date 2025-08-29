9c0f8cba41dcac0da51b468c21f7411bac01f473
imdevan <huapayadevan@gmail.com>
2025-08-28 18:46:35 -0700

    Add generate-client script

---
+++ app/backend/scripts/generate-client.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+# cd app/backend
+python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ./openapi.json
+
+mv openapi.json ../expo/
+
+# Generate expo client
+cd ../expo
+bun generate:client
+bunx biome format --write ./app/client

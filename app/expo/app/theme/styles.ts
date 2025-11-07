61905acf246aab063cfdaa2619c5d33a04cf476c
imdevan <huapayadevan@gmail.com>
2025-11-07 15:04:53 -0800

    Add themed to global styles object

--- app/expo/app/theme/styles.ts
+++ app/expo/app/theme/styles.ts
@@ -1,17 +1,21 @@
-import { ViewStyle } from "react-native"
+import { ViewStyle, TextStyle } from "react-native"
 
-import { spacing } from "./spacing"
+import { ThemedStyle, ThemedStyleArray } from "./types"
+
+export type Styles = Record<string, ThemedStyle<ViewStyle> | ThemedStyle<TextStyle> | ViewStyle | TextStyle>
 
 /* Use this file to define styles that are used in multiple places in your app. */
-export const $styles = {
+export const $styles: Styles = {
   row: { flexDirection: "row" } as ViewStyle,
   flex1: { flex: 1 } as ViewStyle,
   flexWrap: { flexWrap: "wrap" } as ViewStyle,
 
-  container: {
-    paddingTop: spacing.lg + spacing.xl,
+  container: ({spacing, width}) => ({
     paddingHorizontal: spacing.lg,
-  } as ViewStyle,
+    width: width > 1200 ? 800 : width - spacing.lg,
+    margin: "auto",
+    flex: 1,
+  }),
 
   toggleInner: {
     width: "100%",

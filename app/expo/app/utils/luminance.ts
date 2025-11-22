f39c45b616de55e6e5368ef5943de9f813736c09
imdevan <huapayadevan@gmail.com>
2025-11-21 17:32:55 -0800

    add luminance util

---
+++ app/expo/app/utils/luminance.ts
@@ -0,0 +1,10 @@
+export function luminance(hex: string) {
+  const c = hex.replace("#", "");
+  const r = parseInt(c.slice(0, 2), 16) / 255;
+  const g = parseInt(c.slice(2, 4), 16) / 255;
+  const b = parseInt(c.slice(4, 6), 16) / 255;
+  const f = (v: number) =>
+    v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+  return 0.2126 * f(r) + 0.7152 * f(g) + 0.0722 * f(b);
+}
+

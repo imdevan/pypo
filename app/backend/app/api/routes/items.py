bf8d693181f85f9da5bfdb024bf69e0fed602548
imdevan <huapayadevan@gmail.com>
2025-12-11 13:12:01 -0800

    replace route uuids with strings

--- app/backend/app/api/routes/items.py
+++ app/backend/app/api/routes/items.py
@@ -44,7 +44,7 @@ def read_items(
 
 
 @router.get("/{id}", response_model=ItemPublic)
-def read_item(session: SessionDep, current_user: CurrentUser, id: uuid.UUID) -> Any:
+def read_item(session: SessionDep, current_user: CurrentUser, id: str) -> Any:
     """
     Get item by ID.
     """
@@ -72,7 +72,7 @@ def update_item(
     *,
     session: SessionDep,
     current_user: CurrentUser,
-    id: uuid.UUID,
+    id: str,
     item_in: ItemUpdate,
 ) -> Any:
     """
@@ -89,7 +89,7 @@ def update_item(
 
 @router.delete("/{id}")
 def delete_item(
-    session: SessionDep, current_user: CurrentUser, id: uuid.UUID
+    session: SessionDep, current_user: CurrentUser, id: str
 ) -> Message:
     """
     Delete an item.
